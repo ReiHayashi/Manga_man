09159b77cb685688b2611d38b03da696cd01fe8c
bigdisappointment <bigdisappointment@users.noreply.github.com>
2018-03-09 14:47:32 +0200

    fixed login

--- register.php
+++ register.php
@@ -6,23 +6,23 @@
 		<title>Register</title>
 	</head>
 	<body>
-		<form action="register" method="POST">
-			<input  type="text" name="username" placeholder="username">
-			<input type="email" name="email" placeholder="e-mail">
-			<input type="password" name="password" placeholder="password">
-			<input type="password" name="password2" placeholder="confirm password">
+		<form action="register.php" method="POST">
+			<input  type="text" name="username" placeholder="username"><br>
+			<input type="email" name="email" placeholder="e-mail"><br>
+			<input type="password" name="password" placeholder="password"><br>
+			<input type="password" name="password2" placeholder="confirm password"><br>
 			<input type="submit" name="submit" value="REGISTER">
 		</form>
 
 		<?php
 
-		if(isset($_POST['username']) && isset($_POST['parool'])) {
+		if(isset($_POST['username']) && isset($_POST['password'])) {
 
 			$username = $_POST['username'];
 			$email = $_POST['email'];
 			$password = $_POST['password'];
 			$usertype = 0;
-			$uname_check = "SELECT * FROM users WHERE username='".$username"'";
+			$uname_check = "SELECT * FROM users WHERE username='.$username.'";
 			$result1 = mysqli_query($connection, $uname_check);
 			if(!$result1 and mysqli_num_rows($result1)>0){
 				echo "User already exists";
