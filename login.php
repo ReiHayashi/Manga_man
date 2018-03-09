cb673e8dbd44b2afc94bbf6f7a80af92d53f1a65
bigdisappointment <bigdisappointment@users.noreply.github.com>
2018-03-09 16:05:18 +0200

    this was login oof

--- login.php
+++ login.php
@@ -7,18 +7,17 @@
   </head>
   <body>
     <form class="login" action="login.php" method="POST">
-      <input type="text" name="username" placeholder="Username">
-      <input type="password" name="password" placeholder="Password">
+      <input type="text" name="username" placeholder="Username"> <br>
+      <input type="password" name="password" placeholder="Password"><br>
       <input type="submit" name="submit" value="Login">
     </form>
     <?php
-
-    if(isset($_POST['submit']) && !empty($_POST['username']) && !empty($_POST[password])) {
+    if(isset($_POST['submit']) && !empty($_POST['username']) && !empty($_POST['password'])) {
       $query = "SELECT * FROM users WHERE username='".$_POST['username']."'";
       $result = mysqli_query($connection, $query);
       $row = mysqli_fetch_array($result);
 
-      if(password_verify($_POST['password']) . $row['password']) {
+      if(password_verify($_POST['password']).$row['password']) {
         if($_POST['username'] == $row['username'] && $row['usertype']==1) {
           $_SESSION['aaa']='admin';
           $_SESSION['name']=$row['username'];
