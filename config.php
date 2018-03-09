7b47488fc5ae22afed8803b8d96dcd96215a25b8
bigdisappointment <bigdisappointment@users.noreply.github.com>
2018-03-09 14:20:58 +0200

    connection fix

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $db_database="mangaman";
 $db_user="root";
 $db_password="";
 
-$connection = mysqli_connect($db_server, $db_database, $db_user, $db_password);
+$connection = mysqli_connect($db_server, $db_user, $db_password, $db_database);
 
 if(!$connection) {
   die('everything is fucked');
