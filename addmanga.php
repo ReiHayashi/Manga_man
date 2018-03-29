76c43a6ac1114c2a455d363379fe4b42631ce029
bigdisappointment <bigdisappointment@users.noreply.github.com>
2018-03-29 09:14:24 +0300

    yes yes yes
    legit spent 15 minutes looking for a logical error, turned out i spelled language wrong fucking christ

--- addmanga.php
+++ addmanga.php
@@ -10,31 +10,38 @@
     session_start();
       if($_SESSION['aaa'] === 'admin') {
     ?>
-    <form action="addmanga.php" method="POST">
+    <form action="" method="POST">
       <input type="text" name="Title" placeholder="Title" required> <br>
       <input type="text" name="Genre" placeholder="Genre" required> <br>
       <input type="text" name="Author" placeholder="Author" required> <br>
       <input type="number" name="Year" value="2000" min="1900" max="2099" required> <br>
       <input type="text" name="Price" placeholder="Price" required> <br>
-      <input type="textarea" name="Description" placeholder="description here max 1000 characters" required> <br>
-      <input type="submit" name="submit" value="Add manga to store" required> <br>
+      <select name="Language"><br>
+      <option value="English">English(default)</option>
+      <option value="Russian">Russian</option>
+      <option value="Japanese">Japanese</option>
+      <option value="French">French</option>
+      </select> <br>
+      <textarea name="Description" placeholder="description here max 1000 characters" required></textarea> <br>
+      <input type="submit" name="submit" value="Add manga to shop" required> <br>
     </form>
     <?php
     		if(isset($_POST['Title']) && isset($_POST['Genre']) && isset($_POST['Author']) && isset($_POST['Year']) &&
-          isset($_POST['Price']) && isset($_POST['Description'])) {
+          isset($_POST['Price']) && isset($_POST['Description']) && isset($_POST['Language'])) {
             $title = $_POST['Title'];
             $genre = $_POST['Genre'];
             $author = $_POST['Author'];
             $year = $_POST['Year'];
             $price = $_POST['Price'];
+            $language = $_POST['Language'];
             $description = $_POST['Description'];
-            $query="INSERT INTO manga (manga_name, manga_genre, manga_creator, manga_year, manga_description, manga_price)
-            VALUES ('$title', '$genre', '$author', '$year', '$description', '$price')";
+            $query="INSERT INTO manga (manga_name, manga_genre, manga_creator, manga_year, manga_description, language, manga_price)
+            VALUES ('$title', '$genre', '$author', '$year', '$description', '$language', '$price')";
             $result = mysqli_query($connection, $query);
             if($result) {
               echo "manga has been added.";
             } else {
-              echo "manga has been failed to added.";
+              echo "something went horribly wrong fuck this fucking project.";
             }
     		}
     } else {
