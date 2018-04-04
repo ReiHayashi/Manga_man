89579859a266e061310d61f1f1f0873d5f04034c
bigdisappointment <bigdisappointment@users.noreply.github.com>
2018-04-04 13:36:27 +0300

    database changes because lol i forgot

--- mangaman.sql
+++ mangaman.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2018 at 11:56 PM
+-- Generation Time: Apr 04, 2018 at 12:35 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
@@ -21,6 +21,98 @@ SET time_zone = "+00:00";
 --
 -- Database: `mangaman`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genres`
+--
+
+CREATE TABLE `genres` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `genres`
+--
+
+INSERT INTO `genres` (`id`, `name`) VALUES
+(1, 'Action'),
+(2, 'Adventure'),
+(3, 'Comdey'),
+(4, 'Drama'),
+(5, 'Slice of Life'),
+(6, 'Fantasy'),
+(7, 'Magic'),
+(8, 'Supernatural'),
+(9, 'Horror'),
+(10, 'Mystery'),
+(11, 'Psychological'),
+(12, 'Romance'),
+(13, 'Sci-Fi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genres_in_mangas`
+--
+
+CREATE TABLE `genres_in_mangas` (
+  `genre_id` int(11) NOT NULL,
+  `manga_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `genres_in_mangas`
+--
+
+INSERT INTO `genres_in_mangas` (`genre_id`, `manga_id`) VALUES
+(1, 5),
+(2, 5),
+(4, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `languages`
+--
+
+CREATE TABLE `languages` (
+  `id` int(11) NOT NULL,
+  `language` varchar(32) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `languages`
+--
+
+INSERT INTO `languages` (`id`, `language`) VALUES
+(1, 'English'),
+(2, 'Russian'),
+(3, 'Japanese'),
+(4, 'Spanish'),
+(5, 'Italian'),
+(6, 'French');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `languages_in_mangas`
+--
+
+CREATE TABLE `languages_in_mangas` (
+  `language_id` int(11) NOT NULL,
+  `manga_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `languages_in_mangas`
+--
+
+INSERT INTO `languages_in_mangas` (`language_id`, `manga_id`) VALUES
+(2, 5),
+(3, 5);
 
 -- --------------------------------------------------------
 
@@ -44,6 +136,28 @@ CREATE TABLE `manga` (
 
 INSERT INTO `manga` (`manga_id`, `manga_name`, `manga_creator`, `manga_date`, `manga_description`, `manga_price`, `image`) VALUES
 (5, 'asdf', 'asdgf', '2222-02-22', 'asdgh', '123', 'avatar-166169.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `support`
+--
+
+CREATE TABLE `support` (
+  `support_id` int(11) NOT NULL,
+  `firstname` varchar(42) CHARACTER SET utf8 NOT NULL,
+  `lastname` varchar(42) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(42) CHARACTER SET utf8 NOT NULL,
+  `problem_title` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `problem` varchar(1000) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `support`
+--
+
+INSERT INTO `support` (`support_id`, `firstname`, `lastname`, `email`, `problem_title`, `problem`) VALUES
+(4, 'That', 'Guy', 'that.guy@email.email', 'Title', 'Problem description right here because lol ');
 
 -- --------------------------------------------------------
 
@@ -74,10 +188,42 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `usertype`) VALUES
 --
 
 --
+-- Indexes for table `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `genres_in_mangas`
+--
+ALTER TABLE `genres_in_mangas`
+  ADD KEY `genre_id` (`genre_id`),
+  ADD KEY `manga_id` (`manga_id`);
+
+--
+-- Indexes for table `languages`
+--
+ALTER TABLE `languages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `languages_in_mangas`
+--
+ALTER TABLE `languages_in_mangas`
+  ADD KEY `language_id` (`language_id`),
+  ADD KEY `manga_id` (`manga_id`);
+
+--
 -- Indexes for table `manga`
 --
 ALTER TABLE `manga`
   ADD PRIMARY KEY (`manga_id`);
+
+--
+-- Indexes for table `support`
+--
+ALTER TABLE `support`
+  ADD PRIMARY KEY (`support_id`);
 
 --
 -- Indexes for table `users`
@@ -90,16 +236,52 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `genres`
+--
+ALTER TABLE `genres`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `languages`
+--
+ALTER TABLE `languages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `manga`
 --
 ALTER TABLE `manga`
   MODIFY `manga_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT for table `support`
+--
+ALTER TABLE `support`
+  MODIFY `support_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `genres_in_mangas`
+--
+ALTER TABLE `genres_in_mangas`
+  ADD CONSTRAINT `genres_in_mangas_ibfk_1` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`id`),
+  ADD CONSTRAINT `genres_in_mangas_ibfk_2` FOREIGN KEY (`manga_id`) REFERENCES `manga` (`manga_id`);
+
+--
+-- Constraints for table `languages_in_mangas`
+--
+ALTER TABLE `languages_in_mangas`
+  ADD CONSTRAINT `languages_in_mangas_ibfk_1` FOREIGN KEY (`language_id`) REFERENCES `languages` (`id`),
+  ADD CONSTRAINT `languages_in_mangas_ibfk_2` FOREIGN KEY (`manga_id`) REFERENCES `manga` (`manga_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
