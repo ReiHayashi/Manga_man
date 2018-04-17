a94b9cae1c18936f6fa9df2d8466819de8f19059
bigdisappointment <bigdisappointment@users.noreply.github.com>
2018-04-17 17:10:02 +0300

    nigegr

--- sale.php
+++ sale.php
@@ -10,10 +10,10 @@ elseif (isset($_SESSION['aaa'])&& $_SESSION['aaa']=='user'){
   include('includes/header.php');
 }
 
-if (isset($_GET['pageno'])) {
-    $pageno = $_GET['pageno'];
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
 } else {
-    $pageno = 1;
+    $page = 1;
 }
 ?>
 
@@ -28,26 +28,20 @@ if (isset($_GET['pageno'])) {
   </div>
   <div class="row">
     <?php
-    $no_of_records_per_page = 10;
-    $offset = ($pageno-1) * $no_of_records_per_page;
+    $no_of_records_per_page = 11;
+    $offset = ($page-1) * $no_of_records_per_page;
     //manga query
     $seriesinvolumes = "SELECT V.*,
     series.primaryname as S, series.author as A
     FROM volumes AS V
     INNER JOIN volumes_in_series AS VIS ON VIS.volume_id = V.id
     INNER JOIN series ON VIS.series_id = series.series_id
-<<<<<<< HEAD
     WHERE V.price<9.98
     ORDER BY V.id ASC
     LIMIT $offset, $no_of_records_per_page";
-
-=======
-    WHERE V.price<10
-    ORDER BY V.id ASC';
->>>>>>> parent of 70e388d... sale
     $resultt = mysqli_query($connection, $seriesinvolumes);
     $total_rows = mysqli_fetch_array($resultt)[0];
-    $total_pages = ceil($total_rows / $no_of_records_per_page);
+    $total_pages = intval($total_rows / $no_of_records_per_page)+2;
     $row_count = 0;
     while ($row = mysqli_fetch_array($resultt)) {
       $row_count++;
@@ -78,22 +72,20 @@ if (isset($_GET['pageno'])) {
   <nav>
 
     <ul class="pagination justify-content-center">
-        <li class="page-item "><a class="page-link" href="?pageno=1">First</a></li>
-        <li class="<?php if($pageno <= 1){ echo 'disabled'; } ?>">
-            <a class="page-link" href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?> " tabindex="-1">Previous</a>
+        <li class="page-item "><a class="page-link" href="?page=1">First</a></li>
+        <li class="<?php if($page <= 1){ echo 'disabled'; } ?>">
+            <a class="page-link" href="<?php if($page <= 1){ echo '#'; } else { echo "?page=".($page - 1); } ?> " tabindex="-1">Previous</a>
         </li>
       </li>
+
       <?php
-      for($i=1;$i<$total_pages;$i++){?>
-        <li class="page-item "><a class="page-link" href="?pageno=<?php echo $i;?>"><?php echo $i; ?></a></li>
-      <?php}?>
-      <?php for($i=1; $i<$total_pages; $i++) {
-        echo '';
+      for($i=1; $i<$total_pages; $i++) {
+        echo '<li class="page-item"><a class="page-link" href="?page='.$i.'">'.$i.'</a></li>';
       } ?>
-      <li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?> page-item">
-          <a class="page-link" href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?>">Next</a>
+      <li class="<?php if($page >= $total_pages){ echo 'disabled'; } ?> page-item">
+          <a class="page-link" href="<?php if($page >= $total_pages){ echo '#'; } else { echo "?page=".($page + 1); } ?>">Next</a>
       </li>
-    <li class="page-item"><a class="page-link"  href="?pageno=<?php echo $total_pages; ?>">Last</a></li>
+    <li class="page-item"><a class="page-link"  href="?page=<?php echo $total_pages; ?>">Last</a></li>
     </ul>
   </nav>
   </section>
